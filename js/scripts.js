d91d7338c335794a278c039d4110131e4478a120
karim610 <73453507+karim610@users.noreply.github.com>
2024-12-12 22:28:42 +0100

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+function openModal(img) {
+  // ... (same as before)
+}
+
+function closeModal() {
+  // ... (same as before)
+}
+
+function addToCart() {
+  // Implement your cart functionality here, e.g., updating a cart count, adding to local storage, or sending data to a server.
+  alert("Product added to cart!");
+}
